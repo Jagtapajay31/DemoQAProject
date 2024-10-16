f2dd85dac861c0cd055ebaf3ee8c0d28b5b3bce9
ajay jagtap <jagtapajay64@gmail.com>
2024-10-16 11:39:17 +0530

    new

--- DemoQAProject/src/com/toolsqa/Interactions.java
+++ DemoQAProject/src/com/toolsqa/Interactions.java
@@ -1,6 +1,7 @@
 package com.toolsqa;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,29 +18,40 @@ public class Interactions {
 		System.setProperty("webdriver.chrome.driver","E:\\Tops 2024\\Selenium\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demoqa.com/droppable");
+		driver.get("https://demoqa.com/");
 		Thread.sleep(2000);
 		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0, 300)");
 		
-//		JavascriptExecutor js = (JavascriptExecutor) driver;
-//		js.executeScript("window.scrollBy(0, 300)");
-//		
-//		driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[5]")).click();
-//		Thread.sleep(2000);
+		driver.findElement(By.xpath("//h5[text()='Interactions']")).click();
+		Thread.sleep(2000);
 		
-//		driver.findElement(By.id("item-1")).click();
-//		Thread.sleep(2000);
-		
-//		JavascriptExecutor js1 = (JavascriptExecutor) driver;
-//        WebElement one = driver.findElement(By.xpath("//*[@id=\"demo-tabpane-list\"]/div/div[1]"));
-//        Object scrollupto = null;
-//		js1.executeScript("arguments[0].scrollIntoView();", scrollupto);
-//        Thread.sleep(2000);
-//        new Actions(driver).clickAndHold(one).moveToElement(one).perform();
-//		
-//		driver.findElement(By.id("demo-tab-grid")).click();
+		//Sortable
+//		driver.findElement(By.xpath("//span[text()='Sortable']")).click();
 //		Thread.sleep(2000);
 //		
+//		WebElement one = driver.findElement(By.xpath("//div[text()='One']"));
+//		Thread.sleep(2000);
+//		WebElement five = driver.findElement(By.xpath("//div[text()='Five']"));
+//		Thread.sleep(2000);
+//		
+//		Actions ele1 = new Actions(driver);
+//		ele1.dragAndDrop(one, five).build().perform();
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.xpath("//a[text()='Grid']")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement two = driver.findElement(By.xpath("//*[@id=\"demo-tabpane-grid\"]/div/div/div[2]"));
+//		Thread.sleep(2000);
+//		WebElement nine = driver.findElement(By.xpath("//*[@id=\"demo-tabpane-grid\"]/div/div/div[9]"));
+//		Thread.sleep(2000);
+//		
+//		Actions ele2 = new Actions(driver);
+//		ele2.dragAndDrop(two, nine).build().perform();
+//		Thread.sleep(5000);
+		
 		//Selectable
 		//list
 //		JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -72,7 +84,7 @@ public class Interactions {
 //		
 		
 		//Droppable
-		Actions ele = new Actions(driver);
+//		Actions ele = new Actions(driver);
 //		WebElement drop = driver.findElement(By.id("draggable"));
 //		Thread.sleep(2000);
 //		
@@ -130,24 +142,133 @@ public class Interactions {
 //		Thread.sleep(2000);
 		
 		//Revert
-		driver.findElement(By.id("droppableExample-tab-revertable")).click();
+//		driver.findElement(By.id("droppableExample-tab-revertable")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement revert = driver.findElement(By.id("revertable"));
+//		Thread.sleep(2000);
+//		
+//		WebElement notrevert = driver.findElement(By.id("notRevertable"));
+//		Thread.sleep(2000);
+//		ele.dragAndDrop(revert, notrevert).perform();
+//		Thread.sleep(2000);
+//		
+//		String msg3 = revert.getText();
+//		if(msg3.equals("Dropped!")) {
+//			System.out.println("accept");
+//		}else {
+//			System.out.println("notAcceptable");
+//		}
+		
+		//Resizable
+//		JavascriptExecutor js1 = (JavascriptExecutor) driver;
+//		js1.executeScript("window.scrollBy(0, 300)");
+//		
+//		driver.findElement(By.xpath("//span[text()='Resizable']")).click();
+//		Thread.sleep(2000);
+//		WebElement corner = driver.findElement(By.xpath("//*[@id=\"resizableBoxWithRestriction\"]/span"));
+//		Thread.sleep(2000);
+//		Actions a2 = new Actions(driver);
+//		a2.dragAndDropBy(corner, 50, 50).build().perform();
+//		Thread.sleep(2000);
+//		a2.dragAndDropBy(corner, -100, -100).build().perform();
+		
+		
+		//Droppable
+//		JavascriptExecutor js3 = (JavascriptExecutor) driver;
+//		js3.executeScript("window.scrollBy(0, 300)");
+//		
+//		driver.findElement(By.xpath("//span[text()='Droppable']")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement drag_me = driver.findElement(By.xpath("//div[text()='Drag me']"));
+//		Thread.sleep(2000);
+//
+//		WebElement drop_here = driver.findElement(By.xpath("//p[text()='Drop here']"));
+//		Thread.sleep(2000);
+//
+//		Actions a9 = new Actions(driver);
+//		a9.dragAndDrop(drag_me, drop_here).build().perform();
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.xpath("//a[text()='Accept']")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement acceptable = driver.findElement(By.xpath("//*[@id=\"acceptable\"]"));
+//		Thread.sleep(2000);
+//
+//		WebElement not_acceptable = driver.findElement(By.xpath("//*[@id=\"notAcceptable\"]"));
+//		Thread.sleep(2000);
+//
+//		WebElement drop_here_accept = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]"));
+//		Thread.sleep(2000);
+//
+//		Actions a10 = new Actions(driver);
+//		a10.dragAndDrop(acceptable, drop_here_accept).build().perform();
+//		Thread.sleep(2000);
+//		
+//		a10.dragAndDrop(not_acceptable, drop_here_accept).build().perform();
+//		Thread.sleep(2000);
+//		
+//		driver.findElement(By.xpath("//a[text()='Prevent Propogation']")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement drag_me_prevent = driver.findElement(By.xpath("//*[@id=\"dragBox\"]"));
+//		Thread.sleep(2000);
+//
+//		WebElement drop_here_inner_not_greedy = driver.findElement(By.xpath("//p[text()='Inner droppable (not greedy)']"));
+//		Thread.sleep(2000);
+//
+//		Actions a11 = new Actions(driver);
+//		a11.dragAndDrop(drag_me_prevent, drop_here_inner_not_greedy).build().perform();
+//		Thread.sleep(5000);
+//		
+//		driver.findElement(By.xpath("//a[text()='Revert Draggable']")).click();
+//		Thread.sleep(2000);
+//		
+//		WebElement will_revert = driver.findElement(By.xpath("//*[@id=\"revertable\"]"));
+//		Thread.sleep(2000);
+//
+//		WebElement not_revert = driver.findElement(By.xpath("//*[@id=\"notRevertable\"]"));		
+//		Thread.sleep(2000);
+//
+//		WebElement drop_here_revert = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/div/div[4]/div/div[2]"));
+//		Thread.sleep(2000);
+//
+//		Actions a12 = new Actions(driver);
+//		a12.dragAndDrop(will_revert, drop_here_revert).build().perform();
+//		Thread.sleep(2000);
+//		
+//		a12.dragAndDrop(not_revert, drop_here_revert).build().perform();
+//		Thread.sleep(2000);
+		
+		//Dragabble
+		JavascriptExecutor js4 = (JavascriptExecutor) driver;
+		js4.executeScript("window.scrollBy(0, 300)");
+		
+		driver.findElement(By.xpath("//span[text()='Dragabble']")).click();
 		Thread.sleep(2000);
 		
-		WebElement revert = driver.findElement(By.id("revertable"));
+		WebElement simple_drag_me = driver.findElement(By.xpath("//*[@id=\"dragBox\"]"));
 		Thread.sleep(2000);
 		
-		WebElement notrevert = driver.findElement(By.id("notRevertable"));
-		Thread.sleep(2000);
-		ele.dragAndDrop(revert, notrevert).perform();
+		Actions a13 = new Actions(driver);
+		a13.dragAndDropBy(simple_drag_me, 50, 50).build().perform();
+		driver.findElement(By.xpath("//a[text()='Axis Restricted']")).click();
 		Thread.sleep(2000);
 		
-		String msg3 = revert.getText();
-		if(msg3.equals("Dropped!")) {
-			System.out.println("accept");
-		}else {
-			System.out.println("notAcceptable");
-		}
+		WebElement x_drag_me = driver.findElement(By.xpath("//*[@id=\"restrictedX\"]"));
+		Thread.sleep(2000);
 		
+		WebElement y_drag_me = driver.findElement(By.xpath("//*[@id=\"restrictedY\"]"));
+		Thread.sleep(2000);
+		
+		Actions a14 = new Actions(driver);
+		a14.dragAndDropBy(x_drag_me, 50, 0).build().perform();
+		Thread.sleep(2000);
+		
+		a14.dragAndDropBy(y_drag_me, 0, 100).build().perform();
+		Thread.sleep(2000);
 		
 		Thread.sleep(5000);
 		driver.close();
